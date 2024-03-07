77dbb16080552bc9b9378e2ae6b1f52d2301af2a
unknown <shubhamgorle.me@sbjit.edu.in>
2024-03-07 12:34:59 +0530

    backend almost completed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -62,13 +62,15 @@ exports.getAllOrders = catchAsyncError(async (req, res, next) => {
 // update-order-status--->Admin
 
 exports.updateOrders = catchAsyncError(async (req, res, next) => {
-    const orders = await Order.find(req.params.id);
-
+    const orders = await Order.findById(req.params.id);
+    if (!orders) {
+        return next(new ErrorHandler("Order not found with this id", 404))
+    }
     if (orders.orderStatus === "Delivered") {
         return next(new ErrorHandler("You have already delivered this order", 400))
     }
     orders.orderItems.forEach(async (order) => {
-        await updateStock(order.Product, order.quantity);
+        await updateStock(order.product, order.quantity);
     });
     orders.orderStatus = req.body.status;
     if (req.body.status === "Delivered") {
@@ -87,3 +89,17 @@ async function updateStock(id, quantity) {
     product.stock -= quantity;
     await product.save({ validateBeforeSave: false });
 }
+
+
+// delete order  ---Admin
+exports.deleteOrder = catchAsyncError(async (req, res, next) => {
+    const orders = await Order.findById(req.params.id);
+    if (!orders) {
+        return next(new ErrorHandler("Order not found with this id", 404))
+    }
+    await orders.deleteOne();
+    res.status(200).json({
+        success: true,
+
+    })
+})
